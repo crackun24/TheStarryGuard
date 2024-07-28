13d73194521ccbcc304d16700b833f2d68b2489d
crackun24 <1647923341@qq.com>
2024-07-28 21:17:21 +0800

    修复了命令注册的问题

--- src/main/java/xyz/starrylandserver/thestarryguard/TheStarryGuardFabric.java
+++ src/main/java/xyz/starrylandserver/thestarryguard/TheStarryGuardFabric.java
@@ -4,20 +4,27 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
 import xyz.starrylandserver.thestarryguard.Adapter.FabricAdapter;
+import xyz.starrylandserver.thestarryguard.Commands.CommandsMgr;
 import xyz.starrylandserver.thestarryguard.Events.EventMgr;
 
 public class TheStarryGuardFabric implements ModInitializer {
     FabricAdapter adapter;
     EventMgr eventMgr;//事件管理类
     TgMain main;
+    CommandsMgr mgr;//命令管理对象
     MinecraftServer mc_server;
 
     void regServerStartedEvent() {
+        mgr = new CommandsMgr();
+        mgr.RegAllCommands();//先注册
+
         ServerLifecycleEvents.SERVER_STARTED.register((server -> {
             this.mc_server = server;
             this.adapter = new FabricAdapter(this.mc_server);
             this.main = TgMain.getInstance(adapter);//获取服务主对象的实例
+
             main.start();
+            mgr.setTgMain(main);//后设置对象
 
             this.eventMgr = new EventMgr(this.main);
             eventMgr.RegAllEvent();//注册事件
